91bfd9e99773b74d2544c774b21fda9f656125e1
RiadhJ <jouiniriadh4@gmail.com>
2022-02-19 23:28:43 +0100

    palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,12 @@
+#python program : determine if palindrom
+
+user_input = str(input("Hey user, enter anything and we'll let you know if it's a palindrom :)\n"))
+user_input_reversed = user_input[::-1]
+
+def ispalindrom():
+    if user_input==user_input_reversed:
+        print(f"Yep, it's a palindrom")
+    else:
+        print(f"Nope")
+
+ispalindrom()
