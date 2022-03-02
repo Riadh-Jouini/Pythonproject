f546b7ec56291bcd598346d81d0f505eaf40eb9f
RiadhJ <60111478+RiadhJouini@users.noreply.github.com>
2022-03-02 16:42:38 +0100

    Update removevowels.py

--- removevowels.py
+++ removevowels.py
@@ -1,3 +1,15 @@
+#G#iven a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string. 
+
+#Example 1:
+#Input: s = "leetcodeisacommunityforcoders"
+#Output: "ltcdscmmntyfrcdrs"
+
+#Example 2:
+#Input: s = "aeiou"
+#Output: ""
+
+#Solution:
+
 s = "leetcodeisacommunityforcoders"
 list1=['a', 'e', 'i', 'o','u']
 def removevowels():
