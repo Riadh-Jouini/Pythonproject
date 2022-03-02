8ecb65b8f06d75bd22538e6ca838c8802dffe754
RiadhJ <60111478+RiadhJouini@users.noreply.github.com>
2022-03-02 16:28:44 +0100

    Update Is-subsequence(easy).py

--- Is-subsequence(easy).py
+++ Is-subsequence(easy).py
@@ -1,3 +1,20 @@
+#Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+#A subsequ#ence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+#Example 1:
+
+#Input: s = "abc", t = "ahbgdc"
+#Output: true
+#Example 2:
+
+#Input: s = "axc", t = "ahbgdc"
+#Output: false
+
+#Solution:
+
 input1 = "abc"
 input2 = "bhdcea"
 x = 0
